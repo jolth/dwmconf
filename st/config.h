dcc7002b48efb19e0baa896e0ca8eb075e820fd7
jolth <jolthgs@gmail.com>
2018-04-10 19:01:20 -0500

    font family

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=10:antialias=true:autohint=true";
+/*static char *font = "Liberation Mono:pixelsize=10:antialias=true:autohint=true";*/
+static char *font = "monospace:size=7";
 static int borderpx = 1;
 
 /*

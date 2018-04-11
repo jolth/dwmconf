be9cc8b357ee88ee75624710993120ff31fa0f00
jolth <jolthgs@gmail.com>
2018-04-10 21:47:35 -0500

    update firefox desk

--- config.dwm-6.1.4.h
+++ config.dwm-6.1.4.h
@@ -81,7 +81,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 7,       0,           -1 },
     { "google-chrome-stable",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
